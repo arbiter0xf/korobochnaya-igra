e23ba456c1659474f6df481408b0f183a035fe06
Aleksi Backman <alluzki@protonmail.com>
2024-01-01 13:14:07 +0200

    chore: add initial skeleton for vulkan triangle example

---
+++ experiments/vulkan-hello-triangle/main.cpp
@@ -0,0 +1,49 @@
+#include <vulkan/vulkan.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+class HelloTriangleApplication {
+
+public:
+
+void
+run()
+{
+    initVulkan();
+    mainLoop();
+    cleanup();
+}
+
+private:
+
+void
+initVulkan()
+{
+}
+
+void
+mainLoop()
+{
+}
+
+void
+cleanup()
+{
+}
+
+};
+
+int main(void) {
+    HelloTriangleApplication application;
+
+    try {
+        application.run();
+    } catch(const std::exception& ex) {
+        std::cerr << ex.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
