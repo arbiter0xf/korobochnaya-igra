69f7f86128306e2f3e0be9ff87ca020e34c23496
Aleksi Backman <alluzki@protonmail.com>
2024-01-01 13:34:05 +0200

    feat: enhance vulkan skeleton to spawn a window with custom title

--- experiments/vulkan-hello-triangle/main.cpp
+++ experiments/vulkan-hello-triangle/main.cpp
@@ -1,4 +1,5 @@
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
@@ -11,12 +12,34 @@ public:
 void
 run()
 {
+    initWindow();
     initVulkan();
     mainLoop();
     cleanup();
 }
 
 private:
+
+GLFWwindow* window;
+
+void
+initWindow()
+{
+    const std::string WIN_TITLE  = "Vulkan";
+    const uint32_t    WIN_WIDTH  = 800;
+    const uint32_t    WIN_HEIGHT = 600;
+
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window = glfwCreateWindow(
+            WIN_WIDTH,
+            WIN_HEIGHT,
+            WIN_TITLE.c_str(),
+            nullptr,
+            nullptr);
+}
 
 void
 initVulkan()
@@ -26,11 +49,17 @@ initVulkan()
 void
 mainLoop()
 {
+    while ( ! glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
 }
 
 void
 cleanup()
 {
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
 }
 
 };
